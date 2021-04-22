3fabd97259740a3c5f935379c0e0b65bdc15c860
Rares <tomacherares@gmail.com>
2021-04-22 21:15:08 +0300

    Initial commit

---
+++ src/main/java/com/sda/javaremote18/spring_boot/Application.java
@@ -0,0 +1,29 @@
+package com.sda.javaremote18.spring_boot;
+
+import com.sda.javaremote18.spring_boot.beans.Book;
+import com.sda.javaremote18.spring_boot.config.AppConfig;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.lang.reflect.AnnotatedArrayType;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        context.refresh();
+
+        Book book = context.getBean(Book.class);
+        System.out.println(book.getTitle());
+        System.out.println(book.getAuthor());
+    }
+}
---
+++ src/main/java/com/sda/javaremote18/spring_boot/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.sda.javaremote18.spring_boot.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.sda.javaremote18.spring_boot.beans")
+public class AppConfig {
+
+}
