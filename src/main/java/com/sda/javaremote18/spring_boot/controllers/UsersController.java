8ed4c517779cc8ec9956fc7724af54f3ced6c1e9
Rares <tomacherares@gmail.com>
2021-05-05 19:30:02 +0300

    Update user email delete user

--- src/main/java/com/sda/javaremote18/spring_boot/controllers/UsersController.java
+++ src/main/java/com/sda/javaremote18/spring_boot/controllers/UsersController.java
@@ -20,19 +20,31 @@ public class UsersController {
         this.usersRepository = usersRepository;
     }
 
-//    @GetMapping("/users/welcome") // in acest caz dorim sa afisam continutul paginii HTML welcome
+    //    @GetMapping("/users/welcome") // in acest caz dorim sa afisam continutul paginii HTML welcome
 //    public String showWelcomePage() {
 //        return "welcome"; // welcome reprezinta numele paginii de HTML
 //    }
     @PutMapping("/users/update")
-    public ServerResponse update(@RequestBody UserModel user){
+    public ServerResponse update(@RequestBody UserModel user) {
         Optional<UserModel> checkUser = this.usersRepository.findById(user.getId());
 
-        if(checkUser.isPresent()) {
+        if (checkUser.isPresent()) {
             UserModel userFromDb = checkUser.get();
 
             userFromDb.setFirstName(user.getFirstName());
             userFromDb.setLastName(user.getLastName());
+
+            if (!user.getEmail().equals(userFromDb.getEmail())) {
+                // Verificam daca in baza de date mai avem un utilizator cu noua adresa de email
+                UserModel checkEmail = this.usersRepository.findByEmail(user.getEmail());
+
+                // Asta inseamna ca nu mai exista un cont cu aceasta adresa de email
+                if (checkEmail == null) {
+                    userFromDb.setEmail(user.getEmail());
+                } else {
+                    return new ServerResponse(HttpStatus.BAD_REQUEST.value(), "", "Acest email este folosit de catre un alt utilizator", null);
+                }
+            }
 
             this.usersRepository.save(userFromDb);
 
@@ -40,5 +52,31 @@ public class UsersController {
         } else {
             return new ServerResponse(HttpStatus.BAD_REQUEST.value(), "", "Nu am gasit utilizator", null);
         }
+
     }
+
+    @DeleteMapping("/users/delete/{userId}")
+    public ServerResponse deleteUser(@PathVariable(name = "userId", required = true) String userId) {
+        System.out.println(userId);
+
+        try {
+            Integer id = Integer.parseInt(userId);
+
+            Optional<UserModel> checkUser = this.usersRepository.findById(id);
+
+            if (checkUser.isPresent()) {
+                this.usersRepository.deleteById(id);
+
+                return new ServerResponse(HttpStatus.OK.value(), "User sters cu succes", "", null);
+
+            } else {
+                return new ServerResponse(HttpStatus.OK.value(), "UserID invalid", "", null);
+            }
+
+        } catch (Exception e) {
+            return new ServerResponse(HttpStatus.OK.value(), "UserID invalid", "", null);
+        }
+
+    }
+
 }
